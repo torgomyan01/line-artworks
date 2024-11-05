e0c8f6960ef30a46fd02c0ae0fe03a4189be558f
Andranik Torgomyan <and.torgomyan01@gmail.com>
2024-11-05 20:34:02 +0400

    End home animation omage

--- js/script.js
+++ js/script.js
@@ -16,42 +16,6 @@ function $(name){
   return document.querySelectorAll(name);
 }
 
-const playPause = $el('#play-pause path');
-const sliderContentButtonsPlay = $el('.slider-content-buttons-play');
-
-const playPauseIcons = {
-  play: 'M13.25 8.16065V8.30252C13.25 8.76333 12.9965 9.18676 12.5903 9.40437L2.59028 14.7615C1.75757 15.2076 0.75 14.6043 0.75 13.6597L0.75 2.44636C0.75 1.48658 1.78685 0.884872 2.62017 1.36106L12.6202 7.07534C13.0096 7.29789 13.25 7.71207 13.25 8.16065Z',
-  pause: 'M16.75 2V16C16.75 16.6904 16.1904 17.25 15.5 17.25H2C1.30964 17.25 0.75 16.6904 0.75 16V2C0.75 1.30964 1.30964 0.75 2 0.75L15.5 0.75C16.1904 0.75 16.75 1.30964 16.75 2Z'
-}
-
-let status = false;
-sliderContentButtonsPlay.addEventListener('click', function (){
-  if(status){
-    playPause.setAttribute('d', playPauseIcons.pause)
-  } else {
-    playPause.setAttribute('d', playPauseIcons.play)
-  }
-
-  status = !status;
-})
-
-
-
-const progressBar = $el('.progress-bar');
-
-let percent = 0;
-startProgress();
-function startProgress(){
-  setInterval(() => {
-    percent += 1;
-    progressBar.setAttribute('style', `--percent: ${percent}%`);
-
-    if(percent === 100){
-      percent = 0;
-    }
-  }, 50)
-}
-
 
 
 const sliderInfo = [
@@ -76,13 +40,13 @@ const sliderInfo = [
   {
     type: 'EuroChem Group AG',
     title: 'Image|photoshoot ',
-    img: 'images/slider-image-3.png',
+    img: 'images/slider-image-4.png',
     hashtag: 'Construction'
   },
   {
     type: 'EMCO',
     title: 'Industrial|Photography|at Emco',
-    img: 'images/slider-image-3.png',
+    img: 'images/slider-image-5.png',
     hashtag: 'People'
   },
 ];
@@ -131,6 +95,66 @@ setTimeout(() => {
     item.classList.add(active)
   });
   sliderContentInfoType.classList.add(active);
+  startProgress();
 }, 9000)
 
-console.log(sliderContentImageInfo)
+
+
+const playPause = $el('#play-pause path');
+const sliderContentButtonsPlay = $el('.slider-content-buttons-play');
+const progressBar = $el('.progress-bar');
+
+const playPauseIcons = {
+  play: 'M13.25 8.16065V8.30252C13.25 8.76333 12.9965 9.18676 12.5903 9.40437L2.59028 14.7615C1.75757 15.2076 0.75 14.6043 0.75 13.6597L0.75 2.44636C0.75 1.48658 1.78685 0.884872 2.62017 1.36106L12.6202 7.07534C13.0096 7.29789 13.25 7.71207 13.25 8.16065Z',
+  pause: 'M16.75 2V16C16.75 16.6904 16.1904 17.25 15.5 17.25H2C1.30964 17.25 0.75 16.6904 0.75 16V2C0.75 1.30964 1.30964 0.75 2 0.75L15.5 0.75C16.1904 0.75 16.75 1.30964 16.75 2Z'
+}
+
+let sliderActiveIndex = 0;
+let percent = 0;
+let int;
+
+function startProgress(){
+  int = startAutoLoadingSlider();
+}
+
+
+function startAutoLoadingSlider(){
+  return setInterval(() => {
+    percent += 1;
+    progressBar.setAttribute('style', `--percent: ${percent}%`);
+
+    if(percent === 100){
+      percent = 0;
+      sliderActiveIndex += 1;
+
+      if(sliderActiveIndex === sliderInfo.length - 1){
+        sliderActiveIndex = 0;
+      }
+      nextSlider()
+    }
+  }, 50)
+}
+
+function nextSlider(){
+  console.log(sliderActiveIndex)
+  sliderContentImage.style.backgroundImage = `url(${sliderInfo[sliderActiveIndex].img})`;
+}
+
+
+
+let status = false;
+sliderContentButtonsPlay.addEventListener('click', function (){
+  if(status){
+    playPause.setAttribute('d', playPauseIcons.pause);
+    int = startAutoLoadingSlider();
+    playPause.parentElement.style.marginLeft = '0px';
+  } else {
+    playPause.setAttribute('d', playPauseIcons.play);
+    clearInterval(int);
+    int = undefined;
+    playPause.parentElement.style.marginLeft = '6px';
+  }
+
+  status = !status;
+})
+
